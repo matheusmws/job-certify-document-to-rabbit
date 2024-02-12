76b896efcdeef408765b12d274e7973207c03bc6
Gabriel Andreazza <k.gabriel.andreazza@gmail.com>
2024-02-12 13:48:20 -0300

    refactor: :sparkles: add .env

--- src/configs/databases/KnexConfig.ts
+++ src/configs/databases/KnexConfig.ts
@@ -1,14 +1,13 @@
 import knex from 'knex'
 
-
 const db = knex({
   client: "mysql",
   connection:{
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'admin',
-    database: 'clients'
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE
   }
 })
 
